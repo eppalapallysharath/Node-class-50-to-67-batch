9e755f6e1568245b4ef5e935c677005cda140de0
eppalapallysharath <eppalapallysharath@gmail.com>
2026-02-04 12:46:38 +0530

    added cors

--- connect_with_moongose/app.js
+++ connect_with_moongose/app.js
@@ -5,6 +5,13 @@ const {connectDb} = require("./config/db.js")
 connectDb()
 const {getFruits, addFruits, updateFruits, deleteFruit} = require("./controllers/fruitsController.js")
 const fruitRoutes = require("./routes/fruitsRoutes.js")
+const cors = require("cors")
+
+const corsOptions = {
+    origin:["http://localhost:5173"],
+    methods:["GET","POST", "PUT", "DELETE"]
+}
+app.use(cors(corsOptions))
 
 app.use(express.json())
 app.use(express.urlencoded(true))
